02bdbcd7bf6ad02fe0c87e6920c6627700951d04
Bashar Zheiman <bashar.zheiman@web.de>
2023-11-29 01:15:23 +0000

    Write the main functions for the game

--- run.py
+++ run.py
@@ -1,3 +1,47 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def print_board(board):
+    """Prints the game board."""
+
+def create_board(size):
+    """Creates a new game board of the specified size."""
+
+def place_ship(board):
+    """Randomly places a ship on the game board."""
+
+def get_user_guess(size):
+    """
+    Gets the user's guess for the row and column.
+    Returns a tuple containing the user's guess (row, col).
+    """
+
+def display_ship(board, ship_row, ship_col):
+    """Displays a ship on the game board."""
+
+def display_rules():
+    """Displays the pirate game rules."""
+    print("Ahoy, ye scurvy dog! Welcome to Pirates of the Python!")
+    print("Rules:")
+    print("1. Each player has 2 battleships on the map if the grid size is 5 or less.")
+    print("2. Each player has 5 battleships on the map if the grid size is 6 or more.")
+    print("3. The player wins by sinking all the computer's battleships or using all 10 attempts.")
+    print("4. Ye can choose the grid size of the game, with a maximum size of 8x8.")
+    print("5. Ye can view the game rules.")
+    print("6. Each player takes turns guessing the coordinates to attack.")
+    print("7. If a guess hits a battleship, it's marked as '!' on the board.")
+    print("8. If a guess misses, it's marked as 'X' on the board.")
+    print("Yo-ho-ho! Have fun and good luck, me heartie!")
+
+def get_player_name():
+    """Gets the player's pirate name."""
+
+def get_grid_size():
+    """
+    Gets the grid size from the pirate.
+    Returns an integer representing the grid size.
+    """
+def play_game():
+    """Main function to embark on a pirate adventure."""
+
+# Call the main function directly
+play_game()
